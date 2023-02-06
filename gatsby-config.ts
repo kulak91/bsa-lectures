36eb5e53be3a5e01e5410f812894b0d72955bef7
Dmytro Kulakov <kulakovdmitr@gmail.com>
2023-02-06 16:15:51 +0200

    path prefix fix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,7 +1,7 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-  pathPrefix: `/test-lectures`,
+  pathPrefix: `/bsa-lectures`,
   siteMetadata: {
     title: `test`,
     siteUrl: `https://www.yourdomain.tld`,
