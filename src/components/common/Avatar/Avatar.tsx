1dbb10539163349eaaf197f0a73a2e27f359956a
Dmytro Kulakov <kulakovdmitr@gmail.com>
2023-02-08 13:07:13 +0200

    full author names updated

--- src/components/common/Avatar/Avatar.tsx
+++ src/components/common/Avatar/Avatar.tsx
@@ -18,7 +18,18 @@ import './avatar.css';
 const avatarsMapper = {
   [AuthorNames.VIKTORIIA_VASYLENKO_EN]: viktoriiaVasylenko2021Avatar,
   [AuthorNames.VIKTORIIA_VASYLENKO_UA]: viktoriiaVasylenko2021Avatar,
+  [AuthorNames.VIKTORIIA_VASYLENKO_RU]: viktoriiaVasylenko2021Avatar,
   [AuthorNames.OLEKSANDR_DANYLCHENKO_UA]: oleksandrDanylchenko2021Avatar,
+  [AuthorNames.OLEKSANDR_DANYLCHENKO_EN]: oleksandrDanylchenko2021Avatar,
+  [AuthorNames.OLEKSANDR_DANYLCHENKO_RU]: oleksandrDanylchenko2021Avatar,
+  [AuthorNames.ALEXANDR_TOVMACH]: alexandrTovmach2018Avatar,
+  [AuthorNames.KYRYLO_LESOHORSKYI]: kyryloLesohorskyi2020Avatar,
+  [AuthorNames.NIKITA_KRASNOV]: nikitaKrasnov2018Avatar,
+  [AuthorNames.PAVEL_NEMCHENKO]: pavelNemchenko2017Avatar,
+  [AuthorNames.VOLODYMYR_LENCH]: LenchVolodymyr2021Avatar,
+  [AuthorNames.ROSTYSLAV_DIAKIV]: rostyslavDiakiv2018Avatar,
+  [AuthorNames.ROMAN_SAHAN]: romanSahan2017Avatar,
+  [AuthorNames.VOLODYMYR_KUSHNIR]: volodymyrKushnir2017Avatar,
 };
 
 type AuthorProps = {
