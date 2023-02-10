3f73055d107c03564bcdf8f7d7893f3f55635849
Dmytro Kulakov <kulakovdmitr@gmail.com>
2023-02-10 15:18:06 +0200

    types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,3 @@
-import { ReactNode } from 'react';
-
-export type AccordionData = {
-  title: string;
-  content: ReactNode;
-};
+export { PageContext } from './page-context/page-context';
+export { LecturesQuery, SiteMetaQuery } from './graphql/queries';
+export { ChartDatasets } from './chart/radar-chart';
